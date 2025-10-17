a426f8df161140d4a896a068c1b36b8ec7c7b861
Huib Piguillet <huib@protonmail.com>
2025-10-17 08:42:59 +0200

    create release from workflow

--- scripts/py__bump_package_version.sh
+++ scripts/py__bump_package_version.sh
@@ -1,6 +1,26 @@
-poetry version "$RELEASE_VERSION"
+#!/bin/bash
 
-git add pyproject.toml
-git commit -m "Bump version to $RELEASE_VERSION"
-# git commit --allow-empty -m "Bump version to $RELEASE_VERSION"
-git push
+version_or_rule="${1:-$RELEASE_VERSION}"
+
+old_version="$(poetry version --short)"
+poetry version "$version_or_rule"
+new_version="$(poetry version --short)"
+
+if [[ "$old_version" == "$new_version" ]]; then
+  echo "Version did not change: $new_version"
+  exit 1
+fi
+
+git_push_pyproject () {
+  git add pyproject.toml
+  git commit -m "Bump version: $1 -> $2"
+  git push
+}
+
+git_push_tag () {
+  tag="v$1"
+  git tag "$tag"
+  git push origin "$tag"
+}
+
+git_push_pyproject "$old_version" "$new_version" && git_push_tag "$new_version"
