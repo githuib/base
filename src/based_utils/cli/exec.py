25788e2badb380d4168f27f8ad4a817c2c67fe99
Huib Piguillet <huib@proton.me>
2025-11-24 11:29:16 +0100

    @killed_by_errors

--- src/based_utils/cli/exec.py
+++ src/based_utils/cli/exec.py
@@ -2,7 +2,7 @@ from functools import wraps
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from collections.abc import Callable
+    from collections.abc import Callable, Iterable
 
 
 class FatalError(SystemExit):
@@ -10,33 +10,25 @@ class FatalError(SystemExit):
         super().__init__(" ".join(str(a) for a in ["💀", *args]))
 
 
-def killed_by[E: Exception, **P, T](
-    *errors: type[E],
-) -> Callable[[Callable[P, T]], Callable[P, T]]:
+def killed_by_errors[**P, T](
+    f: Callable[P, T] = None,
+    /,
+    *,
+    errors: Iterable[type[Exception]] = None,
+    unknown_message: str = None,
+) -> Callable[P, T] | Callable[[Callable[P, T]], Callable[P, T]]:
     def decorator(func: Callable[P, T]) -> Callable[P, T]:
         @wraps(func)
         def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
             try:
                 return func(*args, **kwargs)
-            except errors as exc:
+            except tuple(errors or []) as exc:
                 raise FatalError(*exc.args) from exc
-
-        return wrapper
-
-    return decorator
-
-
-def catch_unknown_errors[**P, T](
-    unknown_message: str = "Unknown error",
-) -> Callable[[Callable[P, T]], Callable[P, T]]:
-    def decorator(func: Callable[P, T]) -> Callable[P, T]:
-        @wraps(func)
-        def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
-            try:
-                return func(*args, **kwargs)
             except Exception as exc:
-                raise FatalError(unknown_message) from exc
+                raise FatalError(
+                    *([unknown_message] if unknown_message else exc.args)
+                ) from exc
 
         return wrapper
 
-    return decorator
+    return decorator if f is None else decorator(f)
